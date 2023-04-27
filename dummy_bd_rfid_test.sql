cee20aaf52cc8762ae2bfc511fd13a0994c0ae82
Ignaciodibella <69056299+Ignaciodibella@users.noreply.github.com>
2023-04-27 09:32:34 -0300

    Add files via upload
    Query para la creación de una BD de prueba.
    Crea 2 tablas (Usuarios y Productos) y agrega un par de registros en ellas.

---
+++ dummy_bd_rfid_test.sql
@@ -0,0 +1,30 @@
+/*Dummy DB para probar interacción RFID/BackEnd/BD*/
+CREATE DATABASE PruebaLectorRFID
+
+USE PruebaLectorRFID
+/*Cración de Tablas*/
+CREATE TABLE Usuarios(
+	dni int NOT NULL PRIMARY KEY,
+	nombre varchar(100),
+	apellido varchar(50),
+	UID varchar(10),
+	saldo float,
+	activa bit,
+)
+
+CREATE TABLE Productos(
+	id int IDENTITY(1,1) PRIMARY KEY,
+	nombre varchar(50),
+	precioUnitario float,
+	stock float
+)
+
+INSERT INTO Usuarios
+VALUES (41479427, 'Ignacio', 'Di Bella', '0620B265', 0, 1),
+	   (41479446, 'Agustin', 'Nabarlaz', 'D64DBE32', 0, 1)
+
+INSERT INTO Productos
+VALUES ('Vaso de Fernet', 600, 10),
+	   ('Vaso de Campari', 500, 5),
+	   ('Botella de Fernet', 5000, 1),
+	   ('Botella de Vodka', 3000, 0)
