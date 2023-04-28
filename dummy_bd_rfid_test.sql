3fbf560ca9978acb0449b36d65d5a8274500b25b
Ignaciodibella <69056299+Ignaciodibella@users.noreply.github.com>
2023-04-28 17:03:49 -0300

    Update dummy_bd_rfid_test.sql

--- dummy_bd_rfid_test.sql
+++ dummy_bd_rfid_test.sql
@@ -21,7 +21,8 @@ CREATE TABLE Productos(
 
 INSERT INTO Usuarios
 VALUES (111, 'Ignacio', 'Di Bella', '06 20 B2 65', 0, 1),
-	   (222, 'Agustin', 'Nabarlaz', 'D6 4D BE 32', 0, 1)
+       (222, 'Agustin', 'Nabarlaz', 'D6 4D BE 32', 0, 1), 
+       (333, 'Franco', 'Cosci', '7F C0 49 28', 0, 1)
 
 INSERT INTO Productos
 VALUES ('Vaso de Fernet', 600, 10),
